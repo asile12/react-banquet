f341f9110b9cd1b44ff0d510ec4de74fcd9deeba
Elisa <elisa.coquinati@gmail.com>
2019-12-15 16:09:52 +0900

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -24,5 +24,10 @@ module.exports = {
    mode: "production",
    externals: {
       react: "react",
+      "styled-components": {
+         commonjs: "styled-components",
+         commonjs2: "styled-components",
+         amd: "styled-components",
+      },
    },
 }
